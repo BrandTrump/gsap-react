6b3bfd7fdb9438af43b599338b002ad0aef35cb6
Brandon Trump <brandontrump101@gmail.com>
2023-03-09 22:29:04 +1300

    created navbar component & added some styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <nav>
+        <Link to={"/"} className="links">
+          Home
+        </Link>
+
+        <ul>
+          <li>
+            <Link to={"/star-text-effect"} className="links">
+              Star Text
+            </Link>
+          </li>
+          <li>
+            <Link to={"/marquee"} className="links">
+              Marquee
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
