b031961d0e290f60c99d8afde7555f0790991efd
Brandon Trump <brandontrump101@gmail.com>
2023-04-13 23:17:56 +1200

    feat: created seprate page for text animation to be displayed

---
+++ src/pages/GsapTextAnimationPage.jsx
@@ -0,0 +1,13 @@
+import GsapTextAnimation from "../components/GsapTextAnimation";
+import Navbar from "../components/Navbar";
+
+const GsapTextAnimationPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <GsapTextAnimation />
+    </div>
+  );
+};
+
+export default GsapTextAnimationPage;
